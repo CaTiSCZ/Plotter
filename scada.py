a762941a31b86a74653698db38888f6dacb2ba30
Jakub Arm <Jakub.Arm@vut.cz>
2026-02-10 12:24:29 +0100

    Enlarge receive buffer. Remove info from CCU to console. Update CCU graph line spacing.

--- scada.py
+++ scada.py
@@ -124,7 +124,7 @@ class AsyncSocket:
         self._logger = logging.getLogger(__class__.__name__ if logger.application_logger is None else f'{logger.application_logger}.{__class__.__name__}')
         self.loop = loop
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 16*1024*1024)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 32*1024*1024)
         sock.setblocking(False)
         sock.bind(('0.0.0.0', local_port))
         self.sock,self.queue = sock, asyncio.Queue()
@@ -325,7 +325,7 @@ class Device:
                 samples.append(result_code)
                 errs = list(data[6:10])
                 self.loop.call_soon_threadsafe(self.buffer.extend, t, samples, errs)
-                self._logger.info(f"Dev {self.ip} packetNumber[{order}]: result {result_code}")
+                #self._logger.info(f"Dev {self.ip} packetNumber[{order}]: result {result_code}")
                 return order
 
 # Manager of multiple devices
@@ -924,7 +924,7 @@ class Plotter(QWidget):
 
                     num_bits = dev.channels
                     center = (num_bits - 1) / 2.0
-                    offset_step = 0.05  # vertical spacing between bit lines
+                    offset_step = 0.01  # vertical spacing between bit lines
 
                     for bit_idx in range(num_bits):
                         if bit_idx not in self.ax_result_curves:
@@ -985,7 +985,7 @@ class Plotter(QWidget):
         #self._penetrate_firewall()
         #for i, f in enumerate((self._ping_all, self._register_logger_all, self._get_ids, self._get_clock_config, self._register_all, self._reset_counter)):
         # TODO: add self._leader_changed
-        for i, f in enumerate((self._ping_all, self._get_ids, self._get_clock_config, self._register_all, self._reset_counter)): #, self._register_ccu
+        for i, f in enumerate((self._ping_all, self._get_ids, self._get_clock_config, self._register_all, self._register_ccu, self._reset_counter)):
             QTimer(self).singleShot(i * 100, f)
 
 def main(argv):
