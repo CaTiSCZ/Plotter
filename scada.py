2000adc202ba914b2e5f58460b10bcda9893add7
Jakub Streit <JakubStreit@eaton.com>
2026-06-29 14:29:37 +0200

    Change version to v1.11.2

--- scada.py
+++ scada.py
@@ -38,7 +38,7 @@ from PyQt5.QtWidgets import (
 from PyQt5.QtCore import Qt, QTimer, pyqtSignal
 
 APPLICATION_NAME = 'Eaton FDDS SCADA'
-APPLICATION_VERSION = '1.11.1'
+APPLICATION_VERSION = '1.11.2'
 APPLICATION_TITLE = f"{APPLICATION_NAME} v{APPLICATION_VERSION}"
 
 # Constants
